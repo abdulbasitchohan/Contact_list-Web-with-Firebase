cf21c911cd72e37836da339e288660bf4cfb2ae9
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2026-09-15 18:13:04 +0500

    Add all files

---
+++ script.js
@@ -0,0 +1,67 @@
+var nameInp = document.getElementById("nameInp");
+var emailInp = document.getElementById("emailInp");
+var addBtn = document.getElementById("addBtn");
+// var addBtn = document.getElementById(".btn");
+
+
+// console.log(addBtn);
+
+
+
+
+
+
+
+
+
+
+// {// const setuserlist = async () => {
+// //   var userlist = await firebase.database().ref("users").push().key;
+
+// //   var contactList = {
+// //     Name: nameInp.value,
+// //     Email: emailInp.value,
+// //   };
+
+// //   await firebase.database().ref(userlist).set(contactList);
+// //   alert("add new quiz");
+// // };
+
+// // const setUser = async () => {
+// //   await firebase.database().ref("user").set({
+// //     name: nameInp.value,
+// //     email: emailInp.value,
+// //     createdAt: firebase.database.ServerValue.TIMESTAMP,
+// //   });
+// // };
+
+// // setUser();
+
+// // CONTACT LIST WITH FIREBASE
+
+// // search
+
+// // contact list
+
+// // 1) ui Create
+
+// // email ,username = > add
+
+// // ali           asad
+
+// // ali@gmail.com       safasfasf
+
+// // edit delete      edit delete
+
+// // 2) firebase project create
+
+// // 3) database create
+
+// // 4) keys:
+
+// // html =>add
+
+// // 5)
+
+// // 5) data store : add,edit,delete,
+// }
